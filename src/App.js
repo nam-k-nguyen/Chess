0637dde69ce8b3035a9752e07005d6fb73ee7892
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-16 18:58:34 -0400

    Update App.js
    Add function and button to send a get request to usajobs

--- src/App.js
+++ src/App.js
@@ -1,18 +1,29 @@
 import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
   const [content, setContent] = useState(null)
 
   function getInfo() {
-    
+    axios({
+      method: 'get',
+      url: 'https://developer.usajobs.gov/api/codelist/agencysubelements',
+      
+    }).then((response) => {
+      console.log(response)
+      console.log(response.data)
+      setContent(JSON.stringify(response.data))
+      return response
+    })
   }
   return (
     <div className="App">
       <div className="title">
         Content
       </div>
-      <button className="button" onClick={''}>
+      <button className="button" onClick={getInfo}>
         CLICK
       </button>
       <div className="content">
