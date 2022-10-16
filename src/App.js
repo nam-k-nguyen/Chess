910d1ab88ff5e8e50529e1d522a2f6107d0f34a7
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-16 00:28:17 -0400

    Update App.js
    Add skeleton structure, a state for content and a function to call API

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [content, setContent] = useState(null)
+
+  function getInfo() {
+    
+  }
   return (
     <div className="App">
-      
+      <div className="title">
+        Content
+      </div>
+      <button className="button" onClick={''}>
+        CLICK
+      </button>
+      <div className="content">
+        {content ? content : "NULL"}
+      </div>
     </div>
   );
 }
