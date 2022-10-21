fb9fee11be1b219e9dc8e98b5a2b76c21da6d97e
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-21 01:03:26 -0400

    Update Cell.js
    Make Cell component render icons based on string passed down from Board component. Add row and column data attributes

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,4 +1,24 @@
+import {
+  FaChessBishop as Bishop,
+  FaChessKing as King,
+  FaChessKnight as Knight,
+  FaChessPawn as Pawn,
+  FaChessQueen as Queen,
+  FaChessRook as Rook,
+} from 'react-icons/fa';
+import { getRow, getCol } from '../util';
+
 export default function Cell({ content, cellNumber, color, handleCellClick }) {
+  let display = ''
+  switch (content) {
+    case 'bishop': display = <Bishop />; break;
+    case 'king': display = <King />; break;
+    case 'knight': display = <Knight />; break;
+    case 'pawn': display = <Pawn />; break;
+    case 'queen': display = <Queen />; break;
+    case 'rook': display = <Rook />; break;
+    default: display = ''; break;
+  }
 
   return (
     <div className='cell_container'>
@@ -7,8 +27,10 @@ export default function Cell({ content, cellNumber, color, handleCellClick }) {
             style={{background: color}} 
             onClick={handleCellClick}
             data-cell_number={cellNumber}
+            data-cell_row={getRow(cellNumber)}
+            data-cell_col={getCol(cellNumber)}
         >
-          {content}
+          {display}
         </div>
     </div>
   )
