8608198a3ddff70f0970c4847eb035f2b14b5deb
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-20 20:55:48 -0400

    Update App.js
    Refactor socket code and add comments

--- src/App.js
+++ src/App.js
@@ -3,30 +3,28 @@ import './App.css';
 import io from 'socket.io-client';
 
 function App() {
+  // Set state and constants
   const [socket, setSocket] = useState(null)
   const [socketConnected, setSocketConnected] = useState(false)
-  const socket_url_source = 'https://nestjs-socket-server-production.up.railway.app/'
+  const dev = true;
+  const socket_url = dev ? 'http://localhost:3001/' : 'https://nestjs-socket-server-production.up.railway.app/';
 
   // Establish socket connection
-  useEffect(() => {
-    setSocket(io(socket_url_source));
-  }, [])
+  useEffect(() => { setSocket(io(socket_url)) }, [socket_url])
 
   // Subscribe to the socket event
   useEffect(() => {
     if (!socket) return
-    socket.on('connect', () => {
-      setSocketConnected(socket.connected);
-    })
-    socket.on('disconnect', () => {
-      setSocketConnected(socket.connected);
+    socket.on('connect', () => { setSocketConnected(socket.connected) })
+    socket.on('disconnect', () => { setSocketConnected(socket.connected) })
+    socket.on('cell_click', (data) => {
+      alert(`The server said that we clicked cell ${data}`)
     })
   }, [socket])
 
   // Manage socket connection
   const handleSocketConnection = () => {
-    if (socketConnected) {socket.disconnect()}
-    else {socket.connect()}
+    console.log(socketConnected ? socket.disconnect() : socket.connect());
   }
 
     })
