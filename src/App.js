5ea11141b969b5e4a8b5ad257f58494cddbe9272
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-20 20:54:14 -0400

    Update App.js
    Remove axios and api fetching functionality

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import axios from 'axios';
 import io from 'socket.io-client';
 
 function App() {
-  const [content, setContent] = useState(null)
   const [socket, setSocket] = useState(null)
   const [socketConnected, setSocketConnected] = useState(false)
   const socket_url_source = 'https://nestjs-socket-server-production.up.railway.app/'
@@ -31,34 +29,17 @@ function App() {
     else {socket.connect()}
   }
 
-  function getInfo() {
-    // return null;
-    axios({
-      method: 'get',
-      url: 'https://nestjs-socket-server-production.up.railway.app/',
-      // url: 'https://nestjs-socket-server.vercel.app/',
-      // url: 'https://nestjs-test-ochre.vercel.app/',
-      // url: 'https://developer.usajobs.gov/api/codelist/agencysubelements',
-    }).then((response) => {
-      console.log(response)
-      console.log(response.data)
-      setContent(JSON.stringify(response.data))
-      return response
     })
   }
 
   return (
     <div className="App">
       <div className="title"><b>Connection status:</b> {socketConnected ? 'Connected' : 'Disconnected'}</div>
-      <button className="button" onClick={getInfo}>
-        CLICK
-      </button>
       <input
         type="button"
         className="button"
         value={socketConnected ? 'Disconnect' : 'Connect'}
         onClick={handleSocketConnection} />
-      <div className="content">API content: {content ? content : "NULL"}</div>
     </div>
   );
 }
