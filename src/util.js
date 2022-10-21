0926f677c00f613661068e4755e9eb90e12a4e6d
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-21 01:00:54 -0400

    Update util.js
    Create a giga chonk function for getting possible moves for a piece

--- src/util.js
+++ src/util.js
@@ -5,7 +5,7 @@ export function createArray(end_num) {
 
 // Return the row and col the cell belongs to given its number
 export function getNumCoordinate(num) {
-    return getRow(num).toString() + getCol(num).toString(); 
+    return getRow(num).toString() + getCol(num).toString();
 }
 
 // Return the row the cell belongs to given its number
@@ -30,4 +30,63 @@ export function createArray2(N, elements = '') {
         arr.push(elements);
     }
     return arr
+}
+
+export function getMoves(piece, row, col) {
+    let moves = []
+    row = parseInt(row); col = parseInt(col);
+    
+    if (piece === 'rook') {
+        for (let r = 1; r < row; r++) { moves.push({ row: r, col: col }); }
+        for (let r = row + 1; r <= 8; r++) { moves.push({ row: r, col: col }); }
+        for (let c = 1; c < col; c++) { moves.push({ row: row, col: c }); }
+        for (let c = col + 1; c <= 8; c++) { moves.push({ row: row, col: c }); }
+    }
+    if (piece === 'bishop') {
+        for (let i = 1; i <= 7; i++) {
+            moves.push({ row: row - i, col: col - i })
+            moves.push({ row: row - i, col: col + i })
+            moves.push({ row: row + i, col: col - i })
+            moves.push({ row: row + i, col: col + i })
+        }
+    }
+    if (piece === 'knight') {
+        moves.push({ row: row + 1, col: col + 2 })
+        moves.push({ row: row + 1, col: col - 2 })
+        moves.push({ row: row - 1, col: col + 2 })
+        moves.push({ row: row - 1, col: col - 2 })
+        moves.push({ row: row + 2, col: col + 1 })
+        moves.push({ row: row + 2, col: col - 1 })
+        moves.push({ row: row - 2, col: col + 1 })
+        moves.push({ row: row - 2, col: col - 1 })
+    }
+    if (piece === 'pawn') {
+        moves.push({row: row - 2, col: col})
+        moves.push({row: row - 1, col: col})
+        moves.push({row: row + 1, col: col})
+        moves.push({row: row + 2, col: col})
+    }
+    if (piece === 'king') {
+        moves.push({row: row - 1, col: col})
+        moves.push({row: row + 1, col: col})
+        moves.push({row: row - 1, col: col - 1})
+        moves.push({row: row + 1, col: col - 1})
+        moves.push({row: row - 1, col: col + 1})
+        moves.push({row: row + 1, col: col + 1})
+        moves.push({row: row, col: col - 1})
+        moves.push({row: row, col: col + 1})
+    }
+    if (piece === 'queen') {
+        for (let r = 1; r < row; r++) { moves.push({ row: r, col: col }); }
+        for (let r = row + 1; r <= 8; r++) { moves.push({ row: r, col: col }); }
+        for (let c = 1; c < col; c++) { moves.push({ row: row, col: c }); }
+        for (let c = col + 1; c <= 8; c++) { moves.push({ row: row, col: c }); }
+        for (let i = 1; i <= 7; i++) {
+            moves.push({ row: row - i, col: col - i })
+            moves.push({ row: row - i, col: col + i })
+            moves.push({ row: row + i, col: col - i })
+            moves.push({ row: row + i, col: col + i })
+        }
+    }
+    return moves;
 }
