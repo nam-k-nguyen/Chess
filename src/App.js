81f9bf5167de891e43c3d6065ba4281fac990926
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-22 00:44:13 -0400

    Update App.js
    Import and apply routing

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import { useEffect, useState } from 'react';
+import { Routes, Route, NavLink } from 'react-router-dom';
 import './App.css';
+
+import Matching from './pages/Matching';
+import Setting from './pages/Setting';
+import Play from './pages/Play';
+
 import TestZone from './components/TestZone';
 import Board from './components/Board';
 
+
 function App() {
 
-
   return (
-    <div className="App">
-      <TestZone
-        socketConnected={socketConnected}
-        handleSocketConnection={handleSocketConnection}
-      />
-      <Board appHandleCellClick={appHandleCellClick} />
-    </div>
-  );
+    <>
+      <nav className='navbar'>
+        <NavLink end to="/matching">Matching</NavLink>
+        <NavLink end to="/play">Play</NavLink>
+        <NavLink end to="/setting">Setting</NavLink>
+      </nav>
+
+      <div className="App">
+        <TestZone />
+        <Board />
+      </div>
+
+      <Routes>
+        <Route path="/matching" element={<Matching />} />
+        <Route path="/play" element={<Play />} />
+        <Route path="/setting" element={<Setting />} />
+      </Routes>
+    </>
+  )
 }
 
 export default App;
