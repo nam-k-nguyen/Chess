a4181437e4e00b50e4762a53a44abf14ff777b20
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-22 00:37:26 -0400

    Update SocketContext.js
    Add use effect hooks to establish socket connection and update socket connected state.

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -7,6 +7,25 @@ export function useSocket() { return useContext(SocketContext) }
 export default function SocketProvider({ children }) {
   const [socket, setSocket] = useState(null)
   const [socketConnected, setSocketConnected] = useState(false)
+  const socket_url = {
+    production: 'https://nestjs-socket-server-production.up.railway.app/',
+    dev: 'http://localhost:3001/',
+  };
+
+  // Establish socket connection
+  useEffect(() => {
+    let sk = io(socket_url.dev)
+    console.log('Connecting to socket... ', sk)
+    setSocket(sk)
+  }, [])
+
+  // Update socket connection state when it changes
+  useEffect(() => {
+    if (!socket) return
+    socket.on('connect', () => { setSocketConnected(socket.connected); console.log('Socket connected') })
+    socket.on('disconnect', () => { setSocketConnected(socket.connected); console.log('Socket disconnected') })
+  }, [socket])
+
   function toggleSocketConnection() { socketConnected ? socket.disconnect() : socket.connect() }
 
   return (
