3abb2effa95bcfa477c02c3c5efe266f095a2908
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-22 00:43:45 -0400

    Update App.js
    Remove socket related code since we already put it in socket context

--- src/App.js
+++ src/App.js
@@ -1,34 +1,9 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import io from 'socket.io-client';
 import TestZone from './components/TestZone';
 import Board from './components/Board';
-import { getMoves } from './util';
 
 function App() {
-  // Set state and constants
-  const [socket, setSocket] = useState(null)
-  const [socketConnected, setSocketConnected] = useState(false)
-  const dev = true;
-  const socket_url = dev ? 'http://localhost:3001/' : 'https://nestjs-socket-server-production.up.railway.app/';
-
-  // Establish socket connection
-  useEffect(() => { setSocket(io(socket_url)) }, [socket_url])
-
-  // Subscribe to the socket event
-  useEffect(() => {
-    if (!socket) return
-    socket.on('connect', () => { setSocketConnected(socket.connected) })
-    socket.on('disconnect', () => { setSocketConnected(socket.connected) })
-    socket.on('cell_click', (data) => {
-      alert(`The server said that we clicked cell ${data}`)
-    })
-  }, [socket])
-
-  // Manage socket connection
-  const handleSocketConnection = () => {
-    console.log(socketConnected ? socket.disconnect() : socket.connect());
-  }
 
 
   return (
