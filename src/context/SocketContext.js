ce2587ff78eb56a7f8d652ca26c2521dd8de20c2
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-22 00:36:41 -0400

    Update SocketContext.js
    wrap children component around a context provider and export the context. Pass an object that contains socket, socket connected, and toggle connection into the value of the context provider.

--- src/context/SocketContext.js
+++ src/context/SocketContext.js
@@ -1,7 +1,17 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { io } from 'socket.io-client';
 
+const SocketContext = React.createContext();
+export function useSocket() { return useContext(SocketContext) }
+
 export default function SocketProvider({ children }) {
+  const [socket, setSocket] = useState(null)
+  const [socketConnected, setSocketConnected] = useState(false)
+  function toggleSocketConnection() { socketConnected ? socket.disconnect() : socket.connect() }
+
   return (
+    <SocketContext.Provider value={{ socket, socketConnected, toggleSocketConnection }}>
+      {children}
+    </SocketContext.Provider>
   )
 }
