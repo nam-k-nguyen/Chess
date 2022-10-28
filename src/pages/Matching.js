ab06851825f5e070b282c416c6fd7b751fcc8720
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-28 03:47:54 -0400

    feat: add find match and create room buttons

--- src/pages/Matching.js
+++ src/pages/Matching.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Matching() {
   return (
     <div className='page_container matching_page'>
-        Matching
+      <div className='find_match'>
+        <button id='quick_match'>Quick Match ► </button>
+        <button id='create_room'>Create Room ► </button>
+      </div>
     </div>
   )
 }
