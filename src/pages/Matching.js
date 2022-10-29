41fa273486e1ba37bb5e6c51aa4401c9b568d756
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-10-28 21:20:49 -0400

    feat: import socket & add matching event listener
    Add event listener for quick match and create room buttons

--- src/pages/Matching.js
+++ src/pages/Matching.js
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSocket } from '../context/SocketContext'
 
 export default function Matching() {
+  const { socket } = useSocket();
+  function onQuickMatch() {
+    console.log('Quick Match')
+    socket.emit('quick_match', 'data from quick match', res => {
+      console.log(res);
+    })
+  }
+  function onCreateRoom() {
+    console.log('Create Room')
+    socket.emit('create_room', 'data from create room', res => {
+      console.log(res);
+    })
+  }
   return (
     <div className='page_container matching_page'>
       <div className='find_match'>
-        <button id='quick_match'>Quick Match ► </button>
-        <button id='create_room'>Create Room ► </button>
+        <button id='quick_match' onClick={onQuickMatch}>Quick Match ►</button>
+        <button id='create_room' onClick={onCreateRoom}>Create Room ►</button>
       </div>
     </div>
   )
