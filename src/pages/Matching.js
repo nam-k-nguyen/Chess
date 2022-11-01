8e3ca61ca093b36304a5477942b94c8d51b3cbae
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-11-01 05:25:18 -0400

    feat: allow enter and exit queue
    note: haven't applied queue cancelling logic in backend yet

--- src/pages/Matching.js
+++ src/pages/Matching.js
@@ -3,12 +3,19 @@ import { useSocket } from '../context/SocketContext'
 
 export default function Matching() {
   const { socket } = useSocket();
+  const [queueing, setQueueing] = useState(false);
+
   function onQuickMatch() {
+    const session_id = sessionStorage.getItem('chess_session_id')
+    const event_name = queueing ? 'exit_queue' : 'enter_queue'
     console.log('Quick Match')
     socket.emit('quick_match', 'data from quick match', res => {
       console.log(res);
     })
+    socket.emit(event_name, session_id, res => {console.log(res)})
+    setQueueing(curr => !curr)
   }
+
   function onCreateRoom() {
     console.log('Create Room')
     socket.emit('create_room', 'data from create room', res => {
