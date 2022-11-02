f5307a0579571bc8a9bfe3bd60182049ae589285
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-11-02 01:51:15 -0400

    feat: add 2 methods
    Add a method to toggle cell class and one to render react icons jsx based on chess piece input

--- src/util.js
+++ src/util.js
@@ -1,25 +1,52 @@
+import {
+    FaChessBishop as Bishop,
+    FaChessKing as King,
+    FaChessKnight as Knight,
+    FaChessPawn as Pawn,
+    FaChessQueen as Queen,
+    FaChessRook as Rook,
+} from 'react-icons/fa';
 
 
-export function rowFromCellIndex(num) { return Math.ceil(parseInt(num) / 8) }
-export function colFromCellIndex(num) { return (parseInt(num) - 1) % 8 + 1 }
 
+// CELL IDENTIFIER CONVERTER
 export function rowColToCoord(row, col) {
     let row_coord = 9 - row
     let col_coord = String.fromCharCode(96 + col)
     return row_coord + col_coord;
 }
-
 export function coordToRowCol(coord) {
     let row = 9 - parseInt(coord[0])
     let col = coord.charCodeAt(1) - 96
     return { row: row, col: col }
 }
+export function rowFromCellIndex(num) { return Math.ceil(parseInt(num) / 8) }
+export function colFromCellIndex(num) { return (parseInt(num) - 1) % 8 + 1 }
 
 
+
+// CELL UTILTITY
+export function toggleCellClass(row, col, className) {
+    let el = document.querySelector(`[data-row="${row}"][data-col="${col}"]`)
+    if (el) el.classList.toggle(className)
+}
+export function renderPiece(piece) {
+    let display = ''
+    switch (piece) {
+        case 'bishop': display = <Bishop />; break;
+        case 'king': display = <King />; break;
+        case 'knight': display = <Knight />; break;
+        case 'pawn': display = <Pawn />; break;
+        case 'queen': display = <Queen />; break;
+        case 'rook': display = <Rook />; break;
+        default: display = ''; break;
     }
-    return arr
+    return display
 }
 
+
+
+// GET POSSIBLE MOVES
 export function getMoves(piece, row, col) {
     let moves = []
     row = parseInt(row); col = parseInt(col);
