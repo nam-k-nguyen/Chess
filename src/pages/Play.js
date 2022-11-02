18f1b3345a03b014719cf3bcb7dbb5fbeeb9e984
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-11-01 20:49:14 -0400

    feat: include game control panel in play page

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import Board from '../components/Board'
+import GameControl from '../components/GameControl'
 
 export default function Play() {
   return (
     <div className='page_container play_page'>
         <Board />
+        <GameControl />
     </div>
   )
 }
