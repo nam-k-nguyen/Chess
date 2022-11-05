ecc4600a6f507e4f9ae00e5c5a20fe65554fd8dc
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-11-05 03:37:46 -0400

    feat: move cell click handler to board component
    also add a data property for piece color

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,20 +1,10 @@
+import { renderPiece } from '../util';
 
-import { getMoves, renderPiece, toggleCellClass } from '../util';
+export default function Cell({
+  index, row, col, coordinate, piece, pieceColor, cellColor, handleCellClick
+}) {
 
-export default function Cell({ index, row, col, coordinate, piece, pieceColor, cellColor }) {
-
-  function handleCellClick(e) {
-    let cell = e.currentTarget;
-    let row = cell.dataset.row
-    let col = cell.dataset.col
-
-    getMoves(cell.dataset.piece, row, col).forEach(move => {
-      toggleCellClass(move.row, move.col, 'move_cell')
-    })
-    if (cell.dataset.piece !== 'none') {
-      toggleCellClass(row, col, 'active_cell')
-    }
-  }
+  
 
   return (
     <div className='cell_container'>
@@ -23,6 +13,7 @@ export default function Cell({ index, row, col, coordinate, piece, pieceColor, c
         onClick={handleCellClick}
         style={{ background: cellColor, color: pieceColor }}
         data-piece={piece}
+        data-piece-color={pieceColor}
         data-index={index}
         data-row={row}
         data-col={col}
