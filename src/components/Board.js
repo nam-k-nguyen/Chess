670e021fc189faa15777051022fa2b051593a266
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-11-10 19:07:16 -0500

    refactor: cell click handler
    also get possible moves from server

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,85 +1,34 @@
 import { useState } from 'react';
 import Cell from './Cell';
 import { useSocket } from '../context/SocketContext';
-import { getPossibleMoves, rowColToIndex, toggleCellClass, toggleMultipleCellClass } from '../util';
+import { getPossibleMoves, getSessionID, toggleCellClass, toggleMultipleCellClass } from '../util';
 
 export default function Board() {
-	const { socket, boardArray, setBoardArray } = useSocket()
-	const [prevCell, setPrevCell] = useState(null)
+	const { socket, boardArray } = useSocket()
+	const [prevIndex, setPrevIndex] = useState(null)
+	function toggleHighlight(index) {
+		// const piece = boardArray[index].piece
+		const row = boardArray[index].row
+		const col = boardArray[index].col
+		socket.emit('get_possible_moves', {board: boardArray, index: index}, (possible_moves) => {
+			toggleMultipleCellClass(possible_moves, 'move_cell')
+			toggleCellClass(row, col, 'active_cell')
+		})
+	}
 
 	function handleCellClick(e) {
 		let cell = e.currentTarget;
-		
-		let currPiece = cell.dataset.piece
-		let currPieceColor = cell.dataset.pieceColor
-		let currRow = parseInt(cell.dataset.row)
-		let currCol = parseInt(cell.dataset.col)
-		let currPossibleMoves = getPossibleMoves(currPiece, currRow, currCol)
-		let currIndex = rowColToIndex(currRow, currCol)
-		
-		let cellHasPiece = currPiece !== 'none'
-		console.log('\n\nCLICKED')
-		console.log('current cell:', cell)
-		console.log('\tpiece: ', currPiece)
-		console.log('\tpiece color: ', currPieceColor)
-		console.log('\trow:', currRow)
-		console.log('\tcol:', currCol)
-		console.log('\tpossible moves: ', currPossibleMoves)
-		console.log('\tindex: ', currIndex)
-
-		console.log('previous cell:', prevCell)
-		console.log('\tpiece: ', prevCell ? prevCell.dataset.piece : 'none')
-		console.log('\tpiece color: ', prevCell ? prevCell.dataset.pieceColor : 'none')
-		console.log('\trow:', prevCell ? prevCell.dataset.row : 'none')
-		console.log('\tcol:', prevCell ? prevCell.dataset.col  : 'none')
-		console.log('\tpossible moves:', prevCell ? getPossibleMoves(prevCell.dataset.currPiece, prevCell.dataset.row, prevCell.dataset.col)  : 'none')
-		console.log('\tindex: ', prevCell ? rowColToIndex(prevCell.dataset.row, prevCell.dataset.col) : 'none')
-
-		if (!prevCell) {
-			console.log('there is no prevCell')
-
-			if (!cellHasPiece) { return }
-			if (cellHasPiece) {
-				toggleMultipleCellClass(currPossibleMoves, 'move_cell')
-				toggleCellClass(currRow, currCol, 'active_cell')
-				setPrevCell(cell)
-			}
+		const currIndex = cell.dataset.index
+		const cellHasPiece = cell.dataset.piece !== 'none'
+		if (prevIndex) {
+			const data = { move: { src: prevIndex, dest: currIndex }, session_id: getSessionID() }
+			socket.emit('verify_move', data, res => { console.log(res) })
+			toggleHighlight(prevIndex)
+			setPrevIndex(null)
 		}
-
-		else if (prevCell) {
-			console.log('there is prevCell')
-
-			let prevPiece = prevCell.dataset.piece
-			let prevRow = parseInt(prevCell.dataset.row)
-			let prevCol = parseInt(prevCell.dataset.col)
-			let prevPossibleMoves = getPossibleMoves(prevPiece, prevRow, prevCol)
-			let prevIndex = rowColToIndex(prevRow, prevCol)
-
-			console.log(prevPossibleMoves)
-			
-			// If the clicked cell is included in the possible moves of the previous cell
-			for (let i = 0; i < prevPossibleMoves.length; i++) {
-				if (prevPossibleMoves[i].row === currRow && prevPossibleMoves[i].col === currCol) {
-					console.log(currRow, " = ", currCol)
-					setBoardArray(prev => {
-						let temp = prev
-						temp[currIndex].piece = prevPiece 
-						temp[currIndex].pieceColor =  prev[prevIndex].pieceColor
-						temp[prevIndex].piece = 'none'
-						temp[prevIndex].pieceColor = 'none'
-						console.log('board array', temp)
-						socket.emit('update_board', {
-							board: temp, 
-							session_id: sessionStorage.getItem('chess_session_id')
-						})
-						return temp
-					})
-					break;
-				}
-			}
-			toggleMultipleCellClass(prevPossibleMoves, 'move_cell')
-			setPrevCell(null)
-			toggleCellClass(prevRow, prevCol, 'active_cell')
+		else if (cellHasPiece) {
+			toggleHighlight(currIndex)
+			setPrevIndex(currIndex)
 		}
 	}
 
