cad348d8b349b6629804acd868ef608cab0bb7a4
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-11-10 19:02:47 -0500

    feat: create boilerplate board component

---
+++ src/components/BoilerplateBoard.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BoilerplateBoard() {
+  return (
+    <div className="boilerplate-board-container">
+        Please go to matching to find a match
+    </div>
+  )
+}
