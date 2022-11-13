b7b35738fdde4462e7c31d56f997c82c254e049e
Nam Khanh Nguyen <nguye2nk@mail.uc.edu>
2022-11-13 01:47:29 -0500

    feat: add a out-of-match boilerplate for board

--- src/pages/Play.js
+++ src/pages/Play.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Board from '../components/Board'
 import GameControl from '../components/GameControl'
+import BoilerplateBoard from '../components/BoilerplateBoard'
+import { useSocket } from '../context/SocketContext'
 
 export default function Play() {
+  const { inMatch } = useSocket()
+
   return (
     <div className='page_container play_page'>
-        <Board />
+        {inMatch ? <Board /> : <BoilerplateBoard/>}
         <GameControl />
     </div>
   )
